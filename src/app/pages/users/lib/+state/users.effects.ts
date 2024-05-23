9988623a55c04c8c4106c9ab582f73bacadc9872
ArtKanareyka <2pizzakanareyka@gmail.com>
2024-05-23 16:44:52 +0700

    .

--- src/app/pages/users/lib/+state/users.effects.ts
+++ src/app/pages/users/lib/+state/users.effects.ts
@@ -2,13 +2,11 @@ import { inject } from '@angular/core'
 import { createEffect, ofType, Actions } from '@ngrx/effects'
 import { usersActions } from './users.actions'
 import { catchError, map, of, switchMap } from 'rxjs'
-import { UsersApi } from '../../services/users-api.service'
 import { LocalStorageService } from '../../services/local-storage.service'
 
 export const usersEffect = createEffect(
   () => {
     const actions$ = inject(Actions)
-    const usersApi = inject(UsersApi)
     const localStorageService = inject(LocalStorageService)
 
     return actions$.pipe(
